f3a900fb4527c1aee389be9ae19acbef9e1efabe
orion1160 <snyantiago@gmail.com>
2025-03-31 23:22:49 +0200

    tablica_dynamiczna_commit1

--- laby/sem4/struktury/projekt1/struktury.cpp
+++ laby/sem4/struktury/projekt1/struktury.cpp
@@ -1,1 +1,97 @@
- 
+ #include "struktury.h"
+
+ int dynamicArray::printRozmiar() const{//sprawdzenie rozmiaru
+     return size;
+ }
+
+ int dynamicArray::printPojemnosc() const{ //sprawdzenie max pojemnosci
+     return capacity;
+ }
+
+ dynamicArray::dynamicArray(){//wartosci poczatkowe
+    size = 0;
+    capacity = 100;
+    tab = new int[capacity];
+ }
+
+ dynamicArray::~dynamicArray(){
+    delete[] tab; //wyczyszczenie pamieci
+}
+
+void dynamicArray::addFront(int war){
+    if(size == capacity)
+        resize(capacity*2); //podwojenie pojemnosci w razie koniecznosci
+    
+    for(int i = size; i > 0; i--)
+        tab[i] = tab[i-1];  //przesuniecie elementow w prawo
+
+    tab[0] = war;           //dodanie elementu na poczatek
+    size++;                 //zwiekszenie rozmiaru
+}
+
+void dynamicArray::addBack(int war){
+    if(size == capacity)
+        resize(capacity*2); //podwojenie pojemnosci w razie koniecznosci
+    
+    tab[size] = war;        //dodanie elementu z tylu
+    size++;                 //zwiekszenie rozmiaru
+}
+
+void dynamicArray::addAt(int index, int war){
+    if(index < 0 || index > size)
+        return;             //jesli index jest poza zakresem nie robimy nic
+    
+    if(size == capacity)
+        resize(capacity*2); //podwojenie pojemnosci w razie koniecznosci
+    
+    for(int i = size; i > index; i--)
+        tab[i] = tab[i-1];  //przesuniecie elementow w prawo, zrobienie miejsca na wstawienie
+    
+    tab[index] = war;       //dodanie nowego elementu
+    size++;                 //zwiekszenie rozmiaru
+}
+
+void dynamicArray::removeFront(){
+    if(size == 0)
+        return;             //jesli tablica pusta nie robimy nic
+    
+    for(int i = 0; i < size-1; i++)
+        tab[i] = tab[i+1];  //przesuniecie elementow w lewo
+    
+    size--;                 //zmniejszenie rozmiaru
+}
+
+void dynamicArray::removeBack(){
+    if(size == 0)
+        return;             //jesli tablica pusta nie robimy nic
+    
+    size--;                 //zmniejszenie rozmiaru
+}
+
+void dynamicArray::removeAt(int index){
+    if(index < 0 || index >= size)
+        return;             //jesli index poza zakresem nie robimy nic
+
+    for(int i = index; i < size-1; i++)
+        tab[i] = tab[i+1];  //dosuniecie elementow
+    
+    size--;                 //zmniejszenie rozmiaru
+}
+
+bool dynamicArray::find(int war){
+    for(int i = 0; i < size; i++)
+        if(tab[i] == war)
+            return true;    //jesli znaleziono element zwracamy true
+    
+    return false;           //jesli nie znaleziono zwracamy false
+}
+
+void dynamicArray::resize(int newCapacity){
+    int* tmp = new int[newCapacity]; //nowa tablica
+    for(int i = 0; i < size; i++)
+        tmp[i] = tab[i]; //przepisanie elementow
+    
+    delete[] tab;   //usuniecie starej tablicy
+    tab = tmp;      //przypisanie nowej tablicy
+    capacity = newCapacity; //zmiana pojemnosci
+}
--- laby/sem4/struktury/projekt1/struktury.h
+++ laby/sem4/struktury/projekt1/struktury.h
@@ -1,4 +1,55 @@
 #ifndef STRUKTURY_H
 #define STRUKTURY_H
 
+class dynamicArray{
+    public:
+        dynamicArray();
+        ~dynamicArray();
+            
+        void addFront(int war);
+        void addBack(int war);
+
+        void removeFront();
+        void removeBack();
+
+        void addAt(int index, int war);
+        void removeAt(int index);
+
+        bool find(int war);
+        int printRozmiar() const;
+        int printPojemnosc() const;
+
+    private:
+        int* tab;     
+        int size;     // obecny rozmiar [1,2,3,-,-] ->3
+        int capacity; // calkowita pojemnosc [1,2,3,-,-] ->5
+        void resize(int newCapacity); //zmiana pojemnosci tablicy
+};
+
+struct node{
+    int data;
+    node* link;
+};
+
+class linkedList{
+    public:
+        linkedList();
+        ~linkedList();
+        
+        void addHead(int war);
+        void addTail(int war);
+
+        void removeHead();
+        void removeTail();
+
+        void addAtIndex(int index, int war);
+        void removeAtIndex(int index);
+
+        bool findN(int war);
+
+    private:
+        node* head;
+
+};
+
 #endif
